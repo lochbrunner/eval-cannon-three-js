f2b41f0ccb27c9b828e7dbd49548d1b359deed8f
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2018-07-09 16:06:29 +0200

    Movable camera

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import GLTFLoader from 'three-gltf-loader';
 
+import TrackballControls from 'three-trackballcontrols';
+
 import simple_bin from '../assets/simple.bin';
 import simple from '../assets/simple.gltf';
 
@@ -11,9 +13,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new TrackballControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
 
 {
   const light = new THREE.AmbientLight('0x404040');  // soft white light
@@ -25,15 +30,15 @@ document.body.appendChild(renderer.domElement);
 }
 
 
-// Lines
+// Floor
 {
-  const material = new THREE.LineBasicMaterial({color: 0x0000ff, linewidth: 2});
-  const geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-1, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 1, 0));
-  geometry.vertices.push(new THREE.Vector3(1, 0, 0));
-  const line = new THREE.Line(geometry, material);
-  scene.add(line);
+  const geometry = new THREE.PlaneGeometry(5, 5, 0);
+  const material =
+      new THREE.MeshBasicMaterial({color: 0x808080, side: THREE.DoubleSide});
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotation.x = 90;
+  plane.translateZ(2);
+  scene.add(plane);
 }
 
 // Loading model
@@ -58,9 +63,17 @@ camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 function animate() {
   requestAnimationFrame(animate);
-  model.rotation.x += 0.01;
-  model.rotation.y += 0.01;
+  if (model) {
+    model.rotation.x += 0.01;
+    model.rotation.y += 0.01;
+  }
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+function render() {
   renderer.render(scene, camera);
 }
 
+render();
 animate();
