4c4f47ba463fb01528d931fe1eff5d6f412ba879
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2018-07-09 19:10:43 +0200

    Add physics: Sphere falling on plane

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import * as CANNON from 'cannon';
 import * as THREE from 'three';
 import GLTFLoader from 'three-gltf-loader';
-
 import TrackballControls from 'three-trackballcontrols';
 
 import simple_bin from '../assets/simple.bin';
@@ -17,10 +17,28 @@ let camera;
 let renderer = new THREE.WebGLRenderer();
 let controls;
 let plane;
-let ball;
+let ball = {};
 let model;
+let world;
+
+function copy(source, target) {
+  target.position.x = source.position.x;
+  target.position.y = source.position.y;
+  target.position.z = source.position.z;
+  target.quaternion.x = source.quaternion.x;
+  target.quaternion.y = source.quaternion.y;
+  target.quaternion.z = source.quaternion.z;
+  target.quaternion.w = source.quaternion.w;
+}
 
 function init() {
+  // init cannon
+  world = new CANNON.World();
+  world.gravity.set(0, -2, 0);
+  world.broadphase = new CANNON.NaiveBroadphase();
+  world.solver.iterations = 10
+
+  // init three
   camera = new THREE.PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 1000);
   renderer.shadowMap.enabled = true;
@@ -31,6 +49,7 @@ function init() {
   controls = new TrackballControls(camera, renderer.domElement);
   controls.addEventListener('change', render);
 
+  // Add items
   {
     const light = new THREE.AmbientLight('0x202020');  // soft white light
     scene.add(light);
@@ -45,28 +64,40 @@ function init() {
     scene.add(light);
   }
 
-
   // Floor
   {
     const geometry = new THREE.PlaneGeometry(5, 5, 0);
     const material = new THREE.MeshStandardMaterial(
         {color: 0x808080, side: THREE.DoubleSide});
     plane = new THREE.Mesh(geometry, material);
-    plane.rotation.x = Math.PI / 2;
-    plane.translateZ(2);
+    plane.rotation.x = 3 * Math.PI / 2;
+    plane.translateZ(-2);
     plane.receiveShadow = true;
     scene.add(plane);
+    const shape = new CANNON.Plane();
+    const body = new CANNON.Body({mass: 0});
+
+    copy(plane, body);
+
+    body.addShape(shape);
+    world.addBody(body);
   }
 
   // Ball
   {
-    const geometry = new THREE.SphereGeometry(0.3, 16, 16);
+    const RADIUS = 0.3;
+    const geometry = new THREE.SphereGeometry(RADIUS, 16, 16);
     const material = new THREE.MeshLambertMaterial({color: 0xaf0000});
-    ball = new THREE.Mesh(geometry, material);
-    // ball.translateY(-1.9);
-    ball.castShadow = true;
-    ball.receiveShadow = true;
-    scene.add(ball);
+    ball.mesh = new THREE.Mesh(geometry, material);
+    ball.mesh.translateZ(1);
+    ball.mesh.castShadow = true;
+    ball.mesh.receiveShadow = true;
+    ball.body = new CANNON.Body({mass: 1});
+    const shape = new CANNON.Sphere(RADIUS);
+    ball.body.addShape(shape);
+    copy(ball.mesh, ball.body);
+    world.addBody(ball.body);
+    scene.add(ball.mesh);
   }
 
   // Loading model
@@ -91,18 +122,19 @@ function init() {
 
   camera.position.set(0, 0, 5);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
-  render();
   animate();
 }
 
 function animate() {
   requestAnimationFrame(animate);
   if (model) {
-    // model.rotation.x += 0.01;
-    // model.rotation.y += 0.01;
     model.rotation.z += 0.01;
   }
   controls.update();
+  const TIMESTEP = 1 / 60;
+  world.step(TIMESTEP);
+  ball.mesh.position.copy(ball.body.position);
+  ball.mesh.quaternion.copy(ball.body.quaternion);
   renderer.render(scene, camera);
 }
 
